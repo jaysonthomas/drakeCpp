5ac0ae9bc2fb8a2c2cf1122ebd994afca2c383fe
Jayson Thomas <jayson.j.thomas@gmail.com>
2023-09-19 07:20:06 +0100

    Since drake doesn't use bzlmod yet, it's a pain to setup drake as an external repository in bazel. For this reason, this repo is now setup within drake.

--- helloWorld/simple_continuous_time_system.cc
+++ helloWorld/simple_continuous_time_system.cc
@@ -1,34 +1,3 @@
-/*****************************************************************************
- * Copyright (c) 2017, Massachusetts Institute of Technology.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * * Neither the name of the copyright holder nor the names of its contributors
- *   may be used to endorse or promote products derived from this software
- *   without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *****************************************************************************/
-
 // Simple Continuous Time System Example
 //
 // This is meant to be a sort of "hello world" example for the drake::system
@@ -51,19 +20,23 @@ namespace systems {
 // Simple Continuous Time System
 //   xdot = -x + x³
 //   y = x
-class SimpleContinuousTimeSystem : public drake::systems::LeafSystem<double> {
+class SimpleContinuousTimeSystem : public drake::systems::LeafSystem<double> 
+{
  public:
-  SimpleContinuousTimeSystem() {
+  SimpleContinuousTimeSystem() 
+  {
+    // One output and one state variable.
     DeclareVectorOutputPort("y", drake::systems::BasicVector<double>(1),
                             &SimpleContinuousTimeSystem::CopyStateOut);
-    DeclareContinuousState(1);  // One state variable.
+    DeclareContinuousState(1);  
   }
 
  private:
   // xdot = -x + x³
   void DoCalcTimeDerivatives(
       const drake::systems::Context<double>& context,
-      drake::systems::ContinuousState<double>* derivatives) const override {
+      drake::systems::ContinuousState<double>* derivatives) const override 
+  {
     const double x = context.get_continuous_state()[0];
     const double xdot = -x + std::pow(x, 3.0);
     (*derivatives)[0] = xdot;
@@ -71,7 +44,8 @@ class SimpleContinuousTimeSystem : public drake::systems::LeafSystem<double> {
 
   // y = x
   void CopyStateOut(const drake::systems::Context<double>& context,
-                    drake::systems::BasicVector<double>* output) const {
+                    drake::systems::BasicVector<double>* output) const 
+  {
     const double x = context.get_continuous_state()[0];
     (*output)[0] = x;
   }
